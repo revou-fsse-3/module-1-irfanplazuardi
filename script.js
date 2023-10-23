397ecc59e0d40b6e1a9e80ca74d8d4b3e6bbfb1f
irfanplazuardi <irfanplazuardi@gmail.com>
2023-10-23 08:24:06 +0800

    add validation alert for upload file

--- script.js
+++ script.js
@@ -4,7 +4,9 @@ const phoneInput = document.getElementById("phone");
 const regionSelect = document.getElementById("region");
 const idInput = document.getElementById("id");
 const messageTextarea = document.getElementById("message");
+const uploadFile = document.getElementById("upload-pokedex");
 
+// Alert submit form
 function alertFunction() {
   if (nameInput.value === "" || emailInput.value === "" || phoneInput.value === "" || regionSelect.value === "" || idInput.value === "" || messageTextarea.value === "") {
     alert("Please fill all the fields");
@@ -12,3 +14,14 @@ function alertFunction() {
   } else alert("Form submitted successfully");
   return true;
 }
+
+// Alert upload image
+uploadFile.addEventListener("change", function () {
+  if (this.files.length < 2) {
+    alert("Please upload at least 2 images");
+    this.value = "";
+  } else if (this.files.length > 2) {
+    alert("Do not upload more than 2 images");
+    this.value = "";
+  }
+});
